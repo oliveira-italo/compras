43cf9535b4f0de36a766681bfd1b52fe5488cff5
Italo Henrique <ih.espiritosanto@gmail.com>
2021-06-09 22:58:45 -0300

    query com lista

--- src/main/java/com/udemy/compras/component/QueryGraphQL.java
+++ src/main/java/com/udemy/compras/component/QueryGraphQL.java
@@ -1,7 +1,11 @@
 package com.udemy.compras.component;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.udemy.compras.dto.ClienteDTO;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class QueryGraphQL implements GraphQLQueryResolver {
@@ -14,4 +18,17 @@ public class QueryGraphQL implements GraphQLQueryResolver {
         return a + b;
     }
 
+    public ClienteDTO cliente() {
+        return new ClienteDTO("Ricardo, o Terrível", "r@gmail.com");
+    }
+
+    public List<ClienteDTO> clientes() {
+        return Arrays.asList(
+                new ClienteDTO("Ricardo, o Terrível", "r@gmail.com"),
+                new ClienteDTO("Casanova", "cn@gmail.com"),
+                new ClienteDTO("Don Ruan", "dr@gmail.com"),
+                new ClienteDTO("Eu", "e@gmail.com"),
+                new ClienteDTO("James Bond", "jb@gmail.com")
+        );
+    }
 }
---
+++ src/main/java/com/udemy/compras/dto/ClienteDTO.java
@@ -0,0 +1,13 @@
+package com.udemy.compras.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteDTO {
+    private String nome;
+    private String email;
+}
