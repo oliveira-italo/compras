beec9edd5d41f6c2424b95456de538a74210e377
Italo Henrique <ih.espiritosanto@gmail.com>
2021-06-09 21:39:17 -0300

    query.graphqls

--- src/main/java/com/udemy/compras/component/QueryGraphQL.java
+++ src/main/java/com/udemy/compras/component/QueryGraphQL.java
@@ -1,2 +1,17 @@
-package com.udemy.compras.component;public class QueryGraphQL {
+package com.udemy.compras.component;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueryGraphQL implements GraphQLQueryResolver {
+
+    public String hello() {
+        return "Hello GraphQL";
+    }
+
+    public int soma(int a, int b) {
+        return a + b;
+    }
+
 }
--- src/main/java/com/udemy/compras/controller/HelloController.java
+++ src/main/java/com/udemy/compras/controller/HelloController.java
@@ -1,2 +1,16 @@
-package com.udemy.compras.controller;public class HelloController {
+package com.udemy.compras.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String helloWorld() {
+        return "Hello REST 2!";
+    }
+
 }
