148acf560db9fe99eb2ded0102465b6d51b6efa7
Italo Henrique <ih.espiritosanto@gmail.com>
2021-06-24 17:06:04 -0300

    mutations

--- src/main/java/com/udemy/compras/component/graphql/ClienteGraphQL.java
+++ src/main/java/com/udemy/compras/component/graphql/ClienteGraphQL.java
@@ -1,24 +1,33 @@
 package com.udemy.compras.component.graphql;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.udemy.compras.model.Cliente;
-import com.udemy.compras.repository.ClienteRepository;
+import com.udemy.compras.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
-public class ClienteGraphQL implements GraphQLQueryResolver {
+public class ClienteGraphQL implements GraphQLQueryResolver, GraphQLMutationResolver {
 
     @Autowired
-    private ClienteRepository repository;
+    private ClienteService service;
 
     public Cliente cliente(Long id) {
-        return repository.findById(id).orElse(null);
+        return service.find(id);
     }
 
     public List<Cliente> clientes() {
-        return repository.findAll();
+        return service.findAll();
+    }
+
+    public Cliente saveCliente(Long id, String nome, String email) {
+        return service.save(new Cliente(id, nome, email));
+    }
+
+    public Boolean deleteCliente(Long id) {
+        return service.delete(id);
     }
 }
---
+++ src/main/java/com/udemy/compras/service/ClienteService.java
@@ -0,0 +1,41 @@
+package com.udemy.compras.service;
+
+import com.udemy.compras.model.Cliente;
+import com.udemy.compras.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repository;
+
+    public Cliente find(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<Cliente> findAll() {
+        return repository.findAll();
+    }
+
+    @Transactional
+    public Cliente save(Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+    @Transactional
+    public Boolean delete(Long id) {
+        if(repository.findById(id).isPresent()) {
+            repository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+}
